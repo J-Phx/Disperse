db5424d0fa000d7a46f819f687c7342642bdcbdd
ouzm <wjj315315@gmail.com>
2022-04-13 20:01:55 +0800

    update calc scripts

--- scripts/calucate_amount.py
+++ scripts/calucate_amount.py
@@ -1,4 +1,6 @@
 
+from pprint import pprint
+
 
 def calculate_total_stake_tier(user_id, stake):
     key = ""
@@ -17,6 +19,36 @@ def calculate_W_total():
         W_total += _value * tier_mul[_key]["mul"]
 
 
+def calculate_amount_new(Z, W_total, user_infos):
+    print("====== recur ======")
+    pprint(user_infos)
+    rest_token = 0
+    W_overflow = 0
+    for user_id in user_infos.keys():
+        print(user_infos.keys())
+        if user_infos[user_id]['alloc'] < Y:
+            print("processing :", user_id, user_infos[user_id]['alloc'])
+            mul = tier_mul[user_stake_tier[user_id]]["mul"]
+            user_stake = user_infos[user_id]["stake"]
+            amount = Z * (user_stake * mul)
+            user_infos[user_id]['alloc'] += amount
+            new_amount = user_infos[user_id]['alloc']
+            if(new_amount > Y):
+                rest_token += new_amount - Y
+                W_overflow -= (user_stake * mul)
+                user_infos[user_id]['alloc'] = Y
+
+    if rest_token > 0:
+        print("------ before recur ---")
+        print("rest_token:", rest_token)
+        print("max reached")
+        pprint(user_infos)
+        W_left = W_total + W_overflow
+        Z2 = rest_token / W_left
+        calculate_amount_new(Z2, W_left, user_infos)
+        # amount += Z2 * (user_stake * mul)
+
+
 def calculate_amount(user_id):
     global rest_token, W_overflow
     Z = X / W_total
@@ -32,7 +64,6 @@ def calculate_amount(user_id):
     if rest_token > 0:
         Z2 = rest_token / (W_total + W_overflow)
         amount += Z2 * (user_stake * mul)
-
 
     if amount > Y:
         rest_token += amount - Y
@@ -53,21 +84,21 @@ if __name__ == "__main__":
     tier_stake_totle = {}
     user_stake_tier = {}
     X = 1000000  # 预售token个数
-    Y = 100000*1.25  # 硬封顶
+    Y = 200000   # 硬封顶
     rest_token = 0
     W_total = 0
     W_overflow = 0
 
     user_infos = {
-        "0": {"stake": 10},  # tier1
-        "1": {"stake": 8},  # tier1
-        "8": {"stake": 2},  # tier1
-        "2": {"stake": 25000},  # tier2
-        "3": {"stake": 20000},  # tier2
-        "4": {"stake": 32000},  # tier3
-        "5": {"stake": 60000},  # tier4
-        "6": {"stake": 80000},  # tier4
-        "7": {"stake": 200000},  # tier5
+        "0": {"stake": 10, "alloc": 0},  # tier1
+        "1": {"stake": 8, "alloc": 0},  # tier1
+        "8": {"stake": 2, "alloc": 0},  # tier1
+        "2": {"stake": 25000, "alloc": 0},  # tier2
+        "3": {"stake": 20000, "alloc": 0},  # tier2
+        "4": {"stake": 32000, "alloc": 0},  # tier3
+        "5": {"stake": 60000, "alloc": 0},  # tier4
+        "6": {"stake": 80000, "alloc": 0},  # tier4
+        "7": {"stake": 200000, "alloc": 0},  # tier5
     }
 
     for key, value in user_infos.items():
@@ -78,21 +109,30 @@ if __name__ == "__main__":
     calculate_W_total()
     print(f"总权重: {W_total}")
 
-    amount = calculate_amount(user_id="0")
-    print(f"用户0可分配的额度为: {amount}")
-    amount = calculate_amount(user_id="1")
-    print(f"用户1可分配的额度为: {amount}")
-    amount = calculate_amount(user_id="2")
-    print(f"用户2可分配的额度为: {amount}")
-    amount = calculate_amount(user_id="3")
-    print(f"用户3可分配的额度为: {amount}")
-    amount = calculate_amount(user_id="4")
-    print(f"用户4可分配的额度为: {amount}")
-    amount = calculate_amount(user_id="5")
-    print(f"用户5可分配的额度为: {amount}")
-    amount = calculate_amount(user_id="6")
-    print(f"用户6可分配的额度为: {amount}")
-    amount = calculate_amount(user_id="7")
-    print(f"用户7可分配的额度为: {amount}")
-    amount = calculate_amount(user_id="8")
-    print(f"用户8可分配的额度为: {amount}")
+    Z = X / W_total
+    calculate_amount_new(Z, W_total, user_infos)
+    print("-------final in recur ------")
+    pprint(user_infos)
+    total = 0
+    for user_id in user_infos.keys():
+        total += user_infos[user_id]['alloc']
+    print("total:", total)
+
+    # amount = calculate_amount(user_id="0")
+    # print(f"用户0可分配的额度为: {amount}")
+    # amount = calculate_amount(user_id="1")
+    # print(f"用户1可分配的额度为: {amount}")
+    # amount = calculate_amount(user_id="2")
+    # print(f"用户2可分配的额度为: {amount}")
+    # amount = calculate_amount(user_id="3")
+    # print(f"用户3可分配的额度为: {amount}")
+    # amount = calculate_amount(user_id="4")
+    # print(f"用户4可分配的额度为: {amount}")
+    # amount = calculate_amount(user_id="5")
+    # print(f"用户5可分配的额度为: {amount}")
+    # amount = calculate_amount(user_id="6")
+    # print(f"用户6可分配的额度为: {amount}")
+    # amount = calculate_amount(user_id="7")
+    # print(f"用户7可分配的额度为: {amount}")
+    # amount = calculate_amount(user_id="8")
+    # print(f"用户8可分配的额度为: {amount}")
